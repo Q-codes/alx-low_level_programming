3e7c2febc5f2a9b0446a3d71152395351a37834d
Francis <franciskwafe@gmail.com>
2022-09-08 22:21:37 -0700

    autocommit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main entry point
+*use printf not puts
+*Return (0)
+*program should compile without warning with -Wall gcc option
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
