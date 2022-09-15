489f244397bba15aee938d032328928521c6a784
Francis <franciskwafe@gmail.com>
2022-09-14 21:23:08 -0700

    autocommit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Entry - main.h
+ * print _putchar followed by new line
+ * Return 0
+ */
+
+int main(void)
+{
+	char ch;
+	ch=getchar("_putchar");
+	putchar(ch);
+
+	Return 0
+}
