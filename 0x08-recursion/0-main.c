16b7d790554de2210346bb352b0c18c3f1f99490
Francis <franciskwafe@gmail.com>
2022-09-28 20:47:13 -0700

    2-strlen-recursion

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include <stdio.h>
+
 
 
 /**
@@ -12,7 +14,13 @@ int main(void)
 
 {
 
-	_print_rev_recursion("\nColton Walker");
+	int n;
+
+
+
+	n = _strlen_recursion("Corbin Coleman");
+
+	printf("%d\n", n);
 
 	return (0);
 
