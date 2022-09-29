6819dbbbc8fba7236d9398199b43b7d5df0b5be4
Francis <franciskwafe@gmail.com>
2022-09-29 08:48:26 -0700

    Updated function

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,13 @@ int main(void)
 	{
 		putchar(number + '0');
 
-		if (number < '0')
+		if (number == '9')
+			continue;
 
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
