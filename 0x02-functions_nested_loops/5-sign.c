c1de8df2b03645558c110f937218825839061e74
Francis <franciskwafe@gmail.com>
2022-10-22 12:33:41 -0700

    Updated code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,19 +14,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		putchar("+");
 		return (1);
 	}
 
 	if (n == 0)
 	{
-		printf("0");
+		putchar("0");
 		return (0);
 	}
 
 	if (n < 0)
 	{
-		printf("-");
+		putchar("-");
 		return (-1);
 	}
 
