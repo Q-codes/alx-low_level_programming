591719040d247edca3a853425d1e8e97d40fda06
Francis <franciskwafe@gmail.com>
2022-12-26 06:23:47 -0800

    Completed task 3, updated main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers, 0-9 followed by new line
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+
+	_putchar('\n');
+}
