4123a2feb27a996de64d66a06db5e3b164cf7274
Francis <franciskwafe@gmail.com>
2022-12-26 04:31:07 -0800

    Betty styling for task3

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - check if c is digit or not.
+ * _isdigit - check if c is digit or not.
  * @c: variable to check if is digit or not
  *
  * Return: Always 0.
@@ -14,7 +14,7 @@ int _isdigit(int c)
 
 	if (c >= '0' && c <= '9')
 		return (1);
-	
+
 	else
 		return (0);
 }
