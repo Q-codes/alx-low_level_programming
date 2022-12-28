ccafadc2bee51b33a160b0b4acec178565eee942
Francis <franciskwafe@gmail.com>
2022-12-28 01:59:15 -0800

    Updated task 6 code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,15 @@ void print_line(int n)
 {
 	int count;
 
-	if (n <= 0)
-		_putchar('\n');
-
-	else
+	if (n > 0)
+	{
 		for (count = 0; count <= n; count++)
 		{
 
 			_putchar('_');
 		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
 
