7a4b9175da3fc09e32f9fabfb369ce42b9955376
Francis <franciskwafe@gmail.com>
2023-01-01 03:39:26 -0800

    Completed task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - entry
+* @void: takes no function
+* Return: returns nothing
+*/
+
+int main(void)
+{
+
+	long int max, i, j, maxPrime;
+
+	max = 612852475143;
+	maxPrime = 0;
+
+	for (i = 2; i <= max; i++)
+	{
+		for (j = 2; j <= max; j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (i == j && max % i == 0)
+		{
+			if (i > maxPrime)
+			{
+				maxPrime = i;
+			}
+		}
+	}
+	printf("%ld", maxPrime);
+	printf("\n");
+
+	return (0);
+}
